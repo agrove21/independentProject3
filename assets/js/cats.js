74117ab239ccdf2cf285daf2dd28963e07b5ed26
Your Name <you@example.com>
2024-08-07 16:13:53 -0500

    added modal details

--- assets/js/cats.js
+++ assets/js/cats.js
@@ -15,24 +15,46 @@ function fetchCatBreeds() {
     });
 }
 
+function getBreedById(id) {
+  fetch(`${catBaseUrl}/breeds/${id}`)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      showModalData(data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+function showModalData (data) {
+  document.querySelector('.modal-card-title').innerHTML = data.name
+  const modalBody =document.querySelector('.modal-card-body') 
+  const image =`<img src="https://cdn2.thecatapi.com/images/${data.reference_image_id}.jpg"/>`
+  const descript =`<p><strong>Description:</strong> ${data.description}</p>`
+  modalBody.innerHTML = image + descript
+}
+
 function showBreeds(breeds) {
   const breedsContainer = document.querySelector(".breeds-container");
   let buttonsHtml = "";
   for (const breed of breeds) {
-    buttonsHtml += `<button class="button is-light">${breed.name}</button>`;
+    buttonsHtml += `<button id=${breed.id} class="button is-light">${breed.name}</button>`;
   }
   breedsContainer.innerHTML = buttonsHtml;
 
-  document.querySelectorAll('.breeds-container button').forEach(function (btn){
-btn.addEventListener('click', function(){
-    toggleModal()
-})
-  })
+  document.querySelectorAll(".breeds-container button").forEach(function (btn) {
+    btn.addEventListener("click", function (event) {
+      getBreedById(event.target.id)
+      toggleModal();
+    });
+  });
 }
 
-document.querySelector('.delete').addEventListener('click', function(){
-    toggleModal()
-})
+document.querySelector(".delete").addEventListener("click", function () {
+  toggleModal();
+});
 
 function toggleModal() {
   document.getElementById("modal").classList.toggle("is-active");
